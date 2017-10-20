bffbc661959f074552be0f20744f6f15ebb6bc91
Anelc08 <31974262+Anelc08@users.noreply.github.com>
2017-10-19 20:22:51 -0500

    Tarea #1 Año Bisiesto

---
+++ Tarea1/src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        int año;
+        System.out.print("Introduce año: ");
+        año = Integer.parseInt(sc.readLine());
+        if(esBisiesto(año))  //llamada al método
+            System.out.println("Bisiesto");
+        else
+            System.out.println("No es bisiesto");
+    }
+    /**
+     * método que calcula si el  año introducido es o no bisiesto
+     */
+    public static boolean esBisiesto(int a){
+        if(a%4==0 && a%100!=0 || a%400==0)
+            return true;
+        else
+            return false;
+    }
+}
